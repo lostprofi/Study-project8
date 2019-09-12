6d961c3cc6b15ee636b4a5e038d4ea6d50e60f1d
lostprofi <o-lazarchik@mail.ru>
2019-09-12 22:44:58 +0300

    first commit

--- app/js/libs.js
+++ app/js/libs.js
@@ -1,4 +1,10 @@
 const navlist = document.querySelectorAll('.navlist');
+const messageList = document.querySelectorAll('.message');
+const veganBlock = document.querySelector('.vegan');
+const meatBlock = document.querySelector('.meat');
+const restarauntBlock = document.querySelector('.restaraunt');
+
+const headerBlocks = [[meatBlock, messageList[0]], [veganBlock, messageList[1]] , [restarauntBlock, messageList[2]]];
 
 
 navlist.forEach(el=>{
@@ -20,3 +26,59 @@ navlist.forEach(el=>{
         
     });
 })
+
+
+headerBlocks.forEach(el=>{
+    
+    if(el === headerBlocks[1]){
+        el[0].addEventListener('mouseover', ()=>{
+        el[1].style.marginBottom = "112px";
+        el[1].style.marginRight = "34px";
+            el[1].style.opacity = 1;
+        
+        });
+    
+        el[0].addEventListener('mouseout', ()=>{
+        el[1].style.marginBottom = "0";
+        el[1].style.marginRight = "0"; 
+            el[1].style.opacity = 0.5;
+        });
+    }
+    
+    else {
+            el[0].addEventListener('mouseover', ()=>{
+            el[1].style.opacity = 1;
+            el[1].style.marginBottom = "106px";
+                
+                
+        });
+    
+            el[0].addEventListener('mouseout', ()=>{
+            el[1].style.opacity = 0.5;
+            el[1].style.marginBottom = "0";
+            
+        });
+    }
+    
+})
+
+/*headerBlocks.forEach(el=>{
+    
+   
+    
+  
+            el[0].addEventListener('mouseover', ()=>{
+            el[1].style.opacity = 1;
+            el[1].style.marginBottom = "112px";
+                
+                
+        });
+    
+            el[0].addEventListener('mouseout', ()=>{
+            el[1].style.opacity = 0.5;
+            el[1].style.marginBottom = "0";
+            
+        });
+    
+    
+})*/
