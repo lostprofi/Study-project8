15b865abc4cbb764b971c1addf002ea44473db23
lostprofi <o-lazarchik@mail.ru>
2019-09-20 19:00:12 +0300

    fifth commit

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('sass', function(){
 //concat & uglifyjs task
 
 gulp.task('scripts', function(){
-  return gulp.src(['app/js/libs.js'])//берем нужные нам js файлы
+  return gulp.src(['app/js/libs.js', 'node_modules/swiper/dist/js/swiper.min.js'])//берем нужные нам js файлы
   .pipe(concat('libs.min.js'))//собираем их в один файл libs.min.js
   .pipe(babel({
             presets: ['@babel/env'],
